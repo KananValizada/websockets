dbff377b7800b9c72d7abd96208dd75f5ef7bbf3
KananValizada <kenan.veli@yahoo.com>
2022-05-08 17:27:05 +0400

    creating messages

--- app/main.js
+++ app/main.js
@@ -1,4 +1,40 @@
 let socket = io.connect("http://localhost", { forceNew: true });
 socket.on("messages", function (data) {
   console.log(data);
+
+  let html = data
+    .map((d) => {
+      return `<div class='name'>
+                ${d.userName}
+          </div>
+          <a href=${d.content.link} class='message' target=blank>
+            ${d.content.text}
+          </a>`;
+    })
+    .join(" ");
+
+  document.getElementById("messages").innerHTML = html;
+});
+
+const addMessage = (e) => {
+  console.log("yuppi");
+  let payload = {
+    userName: document.getElementById("username").value,
+    content: {
+      text: document.getElementById("message").value,
+      link: document.getElementById("linkAddress").value,
+    },
+    ts: Date.now(),
+  };
+
+  socket.emit("new-message", payload);
+  return false;
+};
+
+let form = document.getElementById("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("aaaa");
+  addMessage(e);
 });
